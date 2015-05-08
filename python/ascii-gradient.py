afa54b456059be5b67d89724694be803cbc84eb3
Leo is awesome <lchan1994@yahoo.com>
2015-05-08 19:29:40 -0400

    drew a nice little drawing

--- python/ascii-gradient.py
+++ python/ascii-gradient.py
@@ -19,8 +19,7 @@ for y in range(h):
             pos = int(len(palette)*(dist(x, y, cx, cy)/cr))
         else:
             d, d1, d2 = dist(x1, y1, x2, y2), dist(x, y, x1, y1), dist(x, y, x2, y2)
-            a = (d1**2+d**2-d2**2)/(2*d1*d) if d1 != 0 else 1
-            dl = d1 * a
-            pos = int(len(palette)*dl/d)
+            a = (d1**2+d**2-d2**2)/(2*d) if d1 != 0 else 1
+            pos = int(len(palette)*a/d)
         line += palette[max(0, min(len(palette)-1, pos))]
     print line
